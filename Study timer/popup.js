52d26e22fd812e15d5ed4c22b0b52e167cd931ec
D4rkisek <106534376+D4rkisek@users.noreply.github.com>
2023-08-27 18:35:43 +0100

    more features added and bugs fixed
    - Bugs fixed
       - The "Start Timer" button will be disabled after it's clicked, preventing it from being clicked again until the timer finishes or is stopped. Once the timer completes or is stopped, the button will be re-enabled, allowing the user to start the timer again if desired.
    
    - New features
       - Added a "Stop Timer" button; by clicking, it will stop the timer if it's currently running.
       - The buttons have different colours and the timer container has rounded corners.

--- Study timer/popup.js
+++ Study timer/popup.js
@@ -1,10 +1,12 @@
 document.addEventListener('DOMContentLoaded', function () {
     const startButton = document.getElementById('startButton');
+    const stopButton = document.getElementById('stopButton');
     const timerDisplay = document.getElementById('timer');
     const notificationSound = document.getElementById('notificationSound');
 
     let timer;
     let timeLeft = 30 * 60; // 30 minutes in seconds
+    let isTimerRunning = false;
 
     function updateTimerDisplay() {
         const minutes = Math.floor(timeLeft / 60);
@@ -13,16 +15,31 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 
     function startTimer() {
-        timer = setInterval(function () {
-            if (timeLeft > 0) {
-                timeLeft--;
-                updateTimerDisplay();
-            } else {
-                clearInterval(timer);
-                notificationSound.play();
-            }
-        }, 1000);
+        if (!isTimerRunning) {
+            isTimerRunning = true;
+            startButton.disabled = true; // Disable the start button
+            timer = setInterval(function () {
+                if (timeLeft > 0) {
+                    timeLeft--;
+                    updateTimerDisplay();
+                } else {
+                    clearInterval(timer);
+                    isTimerRunning = false;
+                    notificationSound.play();
+                    startButton.disabled = false; // Re-enable the start button when the timer finishes
+                }
+            }, 1000);
+        }
+    }
+
+    function stopTimer() {
+        if (isTimerRunning) {
+            clearInterval(timer);
+            isTimerRunning = false;
+            startButton.disabled = false; // Re-enable the start button when stopping the timer
+        }
     }
 
     startButton.addEventListener('click', startTimer);
+    stopButton.addEventListener('click', stopTimer);
 });
