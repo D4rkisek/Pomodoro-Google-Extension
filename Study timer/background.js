b4939ee42b5614334f507190f50b32e60b41ae74
D4rkisek <maruf.f@hotmail.com>
2023-09-04 01:55:03 +0100

    refactored javascript files

--- Study timer/background.js
+++ Study timer/background.js
@@ -1,27 +1,49 @@
-let timer = null;
-let timeLeft = 25 * 60;
+let timerId;
+let timeLeft = 25 * 60; // Initial time: 25 minutes (work interval)
 let isTimerRunning = false;
+let isWorkInterval = true;
 
-chrome.runtime.onInstalled.addListener((request, sender, sendResponse) => {
-    if (request.action === 'startTimer' && !isTimerRunning) {
-        isTimerRunning = true;
-        timer = setInterval(() => {
-            if (timeLeft > 0) {
-                timeLeft--;
-                // Update the timer status in the popup using message passing
-                chrome.runtime.sendMessage({ action: 'updateTimer', timeLeft });
-            } else {
-                clearInterval(timer);
-                isTimerRunning = false;
-                // Send a notification that the timer has finished
-                // You can also play the notification sound here
-                // ...
-            }
-        }, 1000);
-    } else if (request.action === 'stopTimer' && isTimerRunning) {
-        clearInterval(timer);
+function startTimer() {
+  if (!isTimerRunning) {
+    isTimerRunning = true;
+    timerId = setInterval(() => {
+      if (timeLeft > 0) {
+        timeLeft--;
+      } else {
+        clearInterval(timerId);
         isTimerRunning = false;
-        // Send a message to stop the timer in the popup
-        chrome.runtime.sendMessage({ action: 'stopTimer' });
-    }
+        chrome.notifications.create({
+          type: 'basic',
+          iconUrl: 'icon.png', // Replace with the path to your extension's icon
+          title: 'Timer Finished',
+          message: isWorkInterval ? 'Time for a break!' : 'Back to work!',
+        });
+
+        if (isWorkInterval) {
+          timeLeft = 5 * 60; // 5 minutes break interval
+          isWorkInterval = false;
+        } else {
+          timeLeft = 25 * 60; // 25 minutes work interval
+          isWorkInterval = true;
+        }
+      }
+    }, 1000);
+  }
+}
+
+function stopTimer() {
+  if (isTimerRunning) {
+    clearInterval(timerId);
+    isTimerRunning = false;
+    timeLeft = isWorkInterval ? 25 * 60 : 5 * 60; // Reset time based on the interval
+  }
+}
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.action === 'startTimer') {
+    startTimer();
+  } else if (request.action === 'stopTimer') {
+    stopTimer();
+  }
 });
+
